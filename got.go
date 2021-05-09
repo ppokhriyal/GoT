a77925b439ab6097d8982288521828469ebd29f7
ppokhriyal4@gmail.com <ppokhriyal4@gmail.com>
2021-05-09 12:06:45 +0530

    GoT main program

--- got.go
+++ got.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
-	"got/aws"
+	"os"
+	_ "got/aws"
 )
 
+
 func main(){
-	fmt.Println("YAML to Terraform Configuration")
-	a := aws.AwsInit()
-	fmt.Println(a)
+	// passing yaml file name
+	if (len(os.Args) < 2 || len(os.Args) > 2) {
+		fmt.Println("Invalid arguments passed")
+	} else {
+		// check if yaml file is aws or docker
+		if (os.Args[1] != "aws.yaml" && os.Args[1] != "docker.yaml"){
+			fmt.Println("Invalid YAML config file")
+		} else {
+			fmt.Println("Building Terraform Config file from "+os.Args[1]+" ...")
+		}
+	}
 }
