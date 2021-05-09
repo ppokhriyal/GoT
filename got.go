b72bcbaf7343a669267b9d6d57a65f61c1bb3616
ppokhriyal4@gmail.com <ppokhriyal4@gmail.com>
2021-05-09 19:47:54 +0530

    GoT main program

--- got.go
+++ got.go
@@ -6,17 +6,55 @@ import (
 	_ "got/aws"
 )
 
+// validate YAML config file
+func CheckYamlConfig(yamlconfig string) bool {
+	status := true
+
+	if (yamlconfig != "aws.yaml" && yamlconfig != "docker.yaml") {
+		status := false
+		return status
+	} else {
+		// check if the YAML config file exists
+		fileInfo,err := os.Stat(yamlconfig)
+		if (err != nil ){
+			status := false
+			return status
+		} else {
+			if (fileInfo.IsDir()){
+				status := false
+				return status
+			} else {
+				if (fileInfo.Size() == 0) {
+					status := false
+					return status
+				} else {
+					return status
+				}
+			}
+			
+		}
+		
+	}
+
+}
 
 func main(){
-	// passing yaml file name
-	if (len(os.Args) < 2 || len(os.Args) > 2) {
-		fmt.Println("Invalid arguments passed")
+	
+	// validate the command-line args
+	if (len(os.Args) < 2 || len(os.Args) > 2){
+		fmt.Println("Invalid argument passeed.\n"+
+		"Please pass the below YAML config files :\n"+
+	    " \u2713 aws.yaml\n"+
+	    " \u2713 docker.yaml")
 	} else {
-		// check if yaml file is aws or docker
-		if (os.Args[1] != "aws.yaml" && os.Args[1] != "docker.yaml"){
-			fmt.Println("Invalid YAML config file")
+		yamlstatus := CheckYamlConfig(os.Args[1])
+		if (yamlstatus == false) {
+			fmt.Println("Invalid argument passeed.\n"+
+		"Please pass the below YAML config files :\n"+
+	    " \u2713 aws.yaml\n"+
+	    " \u2713 docker.yaml")
 		} else {
-			fmt.Println("Building Terraform Config file from "+os.Args[1]+" ...")
+			fmt.Println("Building Project ")
 		}
 	}
 }
